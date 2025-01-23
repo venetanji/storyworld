1253727650231941f18133144de0fb567ed26294
Giovanni Lion <giovanni.lion@gmail.com>
2025-01-23 15:37:20 +0800

    Add current_summary method and enhance stage output in StoryFlow

--- src/storyworld/main.py
+++ src/storyworld/main.py
@@ -26,17 +26,26 @@ class StoryWorldState(BaseModel):
 class StoryFlow(Flow[StoryWorldState]):
     initial_state = StoryWorldState()
 
+    def current_summary(self):
+        return "\n".join([stage.summary for stage in self.state.stages])
+
     @start()
     def start(self):
         for index, stage_name in enumerate(stages["stages"]):
             inputs = {
                 "characters": "\n".join([character.description for character in self.state.characters]),
                 "stage": f"{stage_name} - {index + 1} of {len(stages['stages'])}",
-                "synopsis": "\n".join([stage.summary for stage in self.state.stages])
+                "synopsis": self.current_summary(),
+                "stages": "\n".join(stages["stages"]),
             }
 
             new_stage = PlotDevelopment().crew().kickoff(inputs=inputs)
             self.state.stages.append(new_stage.pydantic)
+            print("-------- Stage complete --------\n")
+            print("-------- Stage object ----------\n", new_stage.pydantic, "\n\n")
+            print("-------- Story summary ---------\n", self.current_summary(), "\n\n")
+
+
 
         print("Story complete!")
         print("State of the world:", self.state)
