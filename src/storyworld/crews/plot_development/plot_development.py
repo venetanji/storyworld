34af1874cbe8e5fcea61259b775adc89b70c2cad
Giovanni Lion <giovanni.lion@gmail.com>
2025-01-23 15:38:45 +0800

    Refactor PlotDevelopment to integrate new StoryEvents, StageDraft, and ConsistencyCheck models, enhancing task management and context handling

--- src/storyworld/crews/plot_development/plot_development.py
+++ src/storyworld/crews/plot_development/plot_development.py
@@ -1,18 +1,11 @@
 from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task, before_kickoff
-from storyworld.types import Character, StoryEvent, Stage
+from storyworld.types import StoryEvents, ConsistencyCheck, Stage, StageDraft
 from crewai.knowledge.knowledge import Knowledge
 from crewai.knowledge.source.crew_docling_source import CrewDoclingSource
-from langchain_ollama import ChatOllama
-from typing import List
 from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
-
-# import litellm
-# litellm.set_verbose = True
-
-# If you want to run a snippet of code before or after the crew starts, 
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from typing import List
+import os
 
 embedder_config = {
 	"provider": OpenAIEmbeddingFunction(
@@ -22,10 +15,22 @@ embedder_config = {
 	)
 }
 
+llm  = LLM(
+	model=os.getenv("MODEL"),
+	temperature=0.5,
+)
+
+function_calling_llm  = LLM(
+	model=os.getenv("MODEL"),
+	temperature=0.1,
+)
+
+
 storytelling_knowledge_source = CrewDoclingSource(
     file_paths=[
         "https://en.wikipedia.org/wiki/Hero%27s_journey",
 		"https://mythcreants.com/blog/the-eight-character-archetypes-of-the-heros-journey/",
+		"https://tvtropes.org/pmwiki/pmwiki.php/Main/TheHerosJourney",
     ],
 )
 
@@ -35,53 +40,33 @@ knowledge = Knowledge(
 	collection_name="storytelling"
 )
 
-llm  = LLM(
-	model="ollama/llama3.1-8k",
-	base_url="http://thor:11434",
-	temperature=0.6,
-	presence_penalty=0.2,
-	n_ctx=8192,
-	max_completion_tokens=8192
-)
-
-function_calling_llm = LLM(
-	model="ollama/llama3.1-8k",
-	base_url="http://thor:11434",
-	temperature=0.2,
-	num_ctx=8192,
-	max_completion_tokens=8192
-)
-# llm = ChatOllama(
-# 	model="llama3.1-16k",
-# 	base_url="http://thor:11434",
-# 	temperature=0.6,
-# 	num_ctx=16384, # 16384 is the maximum context length for the model
-# )
-
-
 @CrewBase
 class PlotDevelopment():
 	"""PlotDevelopment crew"""
 
-	# Learn more about YAML configuration files here:
-	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
 	agents_config = 'config/agents.yaml'
 	tasks_config = 'config/tasks.yaml'
 
-	# If you would like to add tools to your agents, you can learn more about it here:
-	# https://docs.crewai.com/concepts/agents#agent-tools
+	
+	def story_director(self) -> Agent:
+		return Agent(
+			config=self.agents_config['story_director'],
+			verbose=True,
+			knowledge=knowledge,
+			llm=llm,
+			function_calling_llm=function_calling_llm,
+			allow_delegation=True,
 
+		)
 
 	@agent
 	def creative_writer(self) -> Agent:
 		return Agent(
 			config=self.agents_config['creative_writer'],
 			verbose=True,
-			knowledge=knowledge,
-			allow_delegation=True,
+			llm=llm,
+			function_calling_llm=function_calling_llm,
 
-			llm=llm
 		)
 
 	@agent
@@ -89,54 +74,39 @@ class PlotDevelopment():
 		return Agent(
 			config=self.agents_config['consistency_checker'],
 			verbose=True,
-			knowledge=knowledge,
-			llm=llm
+			llm=llm,
+			function_calling_llm=function_calling_llm,
 		)
 	
-	# def director(self) -> Agent:
-	# 	return Agent(
-	# 		config=self.agents_config['director'],
-	# 		verbose=True,
-	# 		knowledge=knowledge,
-	# 		allow_delegation=True,
-	# 		llm=llm
-	# 	)
-
-	# To learn more about structured task outputs, 
-	# task dependencies, and task callbacks, check out the documentation:
-	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
-	# @task
-	# def create_character(self) -> Task:
-	# 	return Task(
-	# 		config=self.tasks_config['create_character'],
-	# 		output_pydantic=Character,
-	# 		knowledge=knowledge
-	# 	)
+	@task
+	def stage_draft(self) -> Task:
+		return Task(
+			config=self.tasks_config['stage_draft'],
+			output_pydantic=StageDraft,
+		)
 
 	@task
 	def develop_environment(self) -> Task:
 		return Task(
 			config=self.tasks_config['develop_environment'],
-			output_pydantic=StoryEvent,
-			knowledge=knowledge,
-			agent=self.creative_writer()
+			output_pydantic=StoryEvents,
+			context=[self.stage_draft()]
 		)
 	
 	@task
 	def develop_character(self) -> Task:
 		return Task(
 			config=self.tasks_config['develop_character'],
-			output_pydantic=StoryEvent,		
-			agent=self.creative_writer()
-
+			output_pydantic=StoryEvents,		
+			context=[self.stage_draft(),self.develop_environment()]
 		)
 
 	@task
 	def consistency_check(self) -> Task:
 		return Task(
 			config=self.tasks_config['consistency_check'],
-			agent=self.consistency_checker(),
-			#context=[self.develop_environment(), self.develop_character()]
+			output_pydantic=ConsistencyCheck,
+			context=[self.develop_environment(), self.develop_character()],
 		)
 	
 	@task
@@ -144,8 +114,7 @@ class PlotDevelopment():
 		return Task(
 			config=self.tasks_config['stage_writeup'],
 			output_pydantic=Stage,
-			agent=self.creative_writer(),
-			context=[self.develop_environment(), self.develop_character(), self.consistency_check()]
+			context=[self.consistency_check(),self.develop_environment(), self.develop_character()]
 		)
 
 	@crew
@@ -157,14 +126,14 @@ class PlotDevelopment():
 		return Crew(
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks, # Automatically created by the @task decorator
-			#process=Process.sequential,
+			process=Process.hierarchical,
 			verbose=True,
-			memory=True,
+			#memory=True,
 			embedder=embedder_config,
 			knowledge=knowledge,
+			#planning=True,
+			#planning_llm=llm,
+			manager_agent=self.story_director(),
 			function_calling_llm=function_calling_llm,
-			#manager_agent=self.director(),
-			llm=llm,
-			#manager_llm=llm,
-			#process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+			llm=llm
 		)
